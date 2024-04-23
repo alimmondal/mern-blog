d6ef7a8a9a61a7596c394aaba91712c7218f5aa0
alimmondal <italimbd@gmail.com>
2024-04-23 08:41:30 +0600

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
-import SignIn from "./pages/Login";
 import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/login" element={<SignIn />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/dashboard" element={<Dashboard />} />

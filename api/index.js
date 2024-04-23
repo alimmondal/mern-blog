a35281f8b541b7413547125fa9c0571713617a39
alimmondal <italimbd@gmail.com>
2024-04-24 00:07:46 +0600

    auth user

--- api/index.js
+++ api/index.js
@@ -3,10 +3,15 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config()
 
+
 const app = express();
+
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("mongodb connected")
 }).catch((err) => {
@@ -18,3 +23,4 @@ app.listen(3000, ( ) => {
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)

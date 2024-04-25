3f6bf5cf339747b316424d1e789bd543ffc5bc00
alimmondal <italimbd@gmail.com>
2024-04-25 15:27:57 +0600

    footer done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
 import Login from "./pages/Login";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }

f79efaaa25f90ba0ac8a183c0cf43496e78ade71
alimmondal <italimbd@gmail.com>
2024-05-05 16:05:15 +0600

    user api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {  deleteUser, signoutUser, test, updateUser,  } from '../controller/user.controller.js';
+import {  deleteUser, getUsers, signoutUser, test, updateUser,  } from '../controller/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.get('/test', test)
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', verifyToken, signoutUser);
+router.get('/get-users', verifyToken, getUsers);
 
 
 export default router

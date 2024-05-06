5ddfa4e61f54fbaccd0c743a91e516106d4be4f5
alimmondal <italimbd@gmail.com>
2024-05-06 16:44:31 +0600

    call to action

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 // import CallToAction from "../components/CallToAction";
 // import CommentSection from "../components/CommentSection";
 // import PostCard from "../components/PostCard";
@@ -87,9 +88,9 @@ export default function PostPage() {
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
 
-      {/* <div className="max-w-4xl mx-auto w-full">
+      <div className="max-w-4xl mx-auto w-full">
         <CallToAction />
-      </div> */}
+      </div>
       {/* <CommentSection postId={post._id} /> */}
 
       {/* <div className="flex flex-col justify-center items-center mb-5">

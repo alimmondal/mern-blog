d9c7389d2a6af349e2caae5a3223910c883f825e
alimmondal <italimbd@gmail.com>
2024-05-13 06:44:24 +0600

    pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 dotenv.config()
 
@@ -19,6 +20,8 @@ mongoose.connect(process.env.MONGO).then(() => {
   console.log(err)
 })
 
+const __dirname = path.resolve()
+
 app.use(cookieParser());
 
 app.listen(3000, ( ) => {
@@ -30,6 +33,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes)
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use((err,req, res, next) => {
   const statusCode = err.statusCode || 500;
